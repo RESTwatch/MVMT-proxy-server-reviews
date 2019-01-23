7c449d376ac5d95ea66b5ebb93831db704eee768
Paloma Velazquez <paloma.velazquez@gmail.com>
2019-01-22 16:25:25 -0800

    Updated ports, etc. to match reviews component

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ app.use('/watches/:wid', express.static('public'));
 
 // app.use('/api/watches/:wid/photos', proxy({ target: 'http://54.211.73.211', changeOrigin: true }));
 // app.use('/api/watches/:wid/summary', proxy({ target: 'http://54.87.165.253', changeOrigin: true }));
-app.use('/api/watches/:wid/details', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
-// app.use('/api/watches/:wid/reviews', proxy({ target: 'http://18.218.241.2', changeOrigin: true }));
+// app.use('/api/watches/:wid/details', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
+app.use('/api/watches/:wid/reviews', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
 
 app.listen(3000, () => {
   console.log('listening on port 3000...');
